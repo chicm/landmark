685ac9b6a07d56ecf27587ab06a6edba8554769b
chicm <idv2005@gmail.com>
2019-04-24 15:25:13 +0000

    updates

---
+++ recognition/loader2.py
@@ -0,0 +1,216 @@
+#import sys
+import os, cv2, glob
+import numpy as np
+import pandas as pd
+import torch
+import torch.utils.data as data
+from torchvision import datasets, models, transforms
+from PIL import Image
+from sklearn.utils import shuffle
+import random
+#sys.path.append('../recognition')
+import settings
+
+from albumentations import (
+    HorizontalFlip, IAAPerspective, ShiftScaleRotate, CLAHE, RandomRotate90, RandomBrightnessContrast, Resize,
+    Transpose, ShiftScaleRotate, Blur, OpticalDistortion, GridDistortion, HueSaturationValue, RandomSizedCrop,
+    IAAAdditiveGaussianNoise, GaussNoise, MotionBlur, MedianBlur, IAAPiecewiseAffine, VerticalFlip,
+    IAASharpen, IAAEmboss, RandomContrast, RandomBrightness, Flip, OneOf, Compose, RandomGamma, ElasticTransform, ChannelShuffle,RGBShift, Rotate
+)
+
+DATA_DIR = settings.DATA_DIR
+
+def get_classes(num_classes, start_index=0):
+    df = pd.read_csv(os.path.join(DATA_DIR, 'train', 'top203094_classes.csv'))
+    classes = df.classes.values.tolist()[start_index: start_index+num_classes]
+    assert num_classes == len(classes)
+    stoi = { classes[i]: i for i in range(len(classes))}
+    return classes, stoi
+
+def get_filename(img_id, img_dir, test_data=False):
+    if test_data:
+        for i in range(10):
+            fn = os.path.join(img_dir, str(i), '{}.jpg'.format(img_id))
+            if os.path.exists(fn):
+                return fn
+        raise AssertionError('image not found: {}'.format(img_id))
+    else:
+        return os.path.join(img_dir, img_id[0], img_id[1], img_id[2], '{}.jpg'.format(img_id))
+
+train_transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # open images mean and std
+        ])
+
+def img_augment(p=1.):
+    return Compose([
+        RandomSizedCrop((200, 250), 224, 224, p=1.),
+        HorizontalFlip(.5),
+        OneOf([
+                CLAHE(clip_limit=2),
+                IAASharpen(),
+                IAAEmboss(),
+                RandomContrast(),
+                RandomBrightness(),
+            ], p=0.3),
+        #
+        ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=15, p=.75 ),
+        Blur(blur_limit=3, p=.33),
+        OpticalDistortion(p=.33),
+        GridDistortion(p=.33),
+        #HueSaturationValue(p=.33)
+    ], p=p)
+
+def val_aug(p=1.):
+    return Compose([Resize(224, 224)], p=1.)
+
+class ImageDataset(data.Dataset):
+    def __init__(self, df, invert_dict, img_dir, stoi, train_mode=True, test_data=False):
+        #self.input_size = 224
+        self.df = df
+        self.invert_dict = invert_dict
+        self.img_dir = img_dir
+        self.stoi = stoi
+        self.train_mode = train_mode
+        self.test_data = test_data
+        #self.num_labels = 14952 #len(self.invert_dict)
+        if train_mode:
+            self.train_labels = list(invert_dict.keys())
+
+    def get_img(self, img_id):
+        fn = get_filename(img_id, self.img_dir, self.test_data)
+        # cv2 and albumentations
+        img = cv2.imread(fn)
+        #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if self.train_mode:
+            aug = img_augment(p=1.)
+            img = aug(image=img)['image']
+        else:
+            aug = val_aug(p=1.)
+            img = aug(image=img)['image']
+        
+        img = transforms.functional.to_tensor(img)
+        img = transforms.functional.normalize(img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        return img
+
+    def get_random_label(self, excludes=[]):
+        #label = random.randint(0, self.num_labels-1)
+        label = random.choice(self.train_labels)
+        while label in excludes:
+            label = random.choice(self.train_labels)
+        return label
+
+    def __getitem__(self, index):
+        row = self.df.iloc[index]
+        anchor_img_id = row['id']
+
+        if self.test_data:
+            return self.get_img(anchor_img_id)
+
+        anchor_label = row['landmark_id']
+        if not self.train_mode:
+            return [self.get_img(anchor_img_id)], [self.stoi[anchor_label]]
+
+        pos_img_id = random.choice(self.invert_dict[anchor_label])
+
+        neg1_label = self.get_random_label(excludes=[anchor_label])
+        #(anchor_label + random.randint(1, self.num_labels // 2)) % self.num_labels
+        neg2_label = self.get_random_label(excludes=[anchor_label, neg1_label])
+        #(anchor_label + random.randint(self.num_labels // 2, self.num_labels-2)) % self.num_labels
+        neg1_img_id = random.choice(self.invert_dict[neg1_label])
+        neg2_img_id = random.choice(self.invert_dict[neg2_label])
+
+        imgs = [self.get_img(x) for x in [anchor_img_id, pos_img_id, neg1_img_id, neg2_img_id]]
+        
+        return imgs, [self.stoi[x] for x in [anchor_label, anchor_label, neg1_label, neg2_label]]
+
+    def __len__(self):
+        return len(self.df)
+
+    def collate_fn(self, batch):
+        if self.test_data:
+            return torch.stack(batch)
+        else:
+            #imgs = torch.stack([x[0] for x in batch])
+            #labels = torch.tensor([x[1] for x in batch])
+            #return imgs, labels
+
+            batch_size = len(batch)
+            images = []
+            labels = []
+            for b in range(batch_size):
+                if batch[b][0] is None:
+                    continue
+                else:
+                    images.extend(batch[b][0])
+                    labels.extend(batch[b][1])
+            images = torch.stack(images, 0)
+            labels = torch.from_numpy(np.array(labels))
+            return images, labels
+
+def create_invert_dict(df_train_clean):
+    df_invert = df_train_clean.groupby('landmark_id')['id'].apply(' '.join).reset_index()
+    df_invert.landmark_id = pd.to_numeric(df_invert.landmark_id)
+    df_invert['img_list'] = df_invert.id.map(lambda x: x.split(' '))
+    invert_dict = pd.Series(df_invert.img_list.values, index=df_invert.landmark_id).to_dict()
+
+    return invert_dict
+
+def get_train_val_loaders(num_classes, start_index=0, batch_size=4, dev_mode=False, val_num=1000, val_batch_size=1024):
+    classes, stoi = get_classes(num_classes, start_index=start_index)
+
+    df_all = pd.read_csv(os.path.join(DATA_DIR, 'train', 'train.csv'))
+    df = shuffle(df_all[df_all.landmark_id.isin(set(classes))].copy().sort_values(by='id'), random_state=1234)
+    print(df.shape)
+    #print(df.head())
+
+    #df = shuffle(pd.read_csv(os.path.join(DATA_DIR, 'train', 'train.csv')), random_state=1234)
+    #df_invert = pd.read_csv(os.path.join(DATA_DIR, 'train_clean_invert.csv'))
+    #df_invert['img_list'] = df_invert.id.map(lambda x: x.split(' '))
+    #invert_dict = pd.Series(df_invert.img_list.values, index=df_invert.landmark_id).to_dict()
+
+    split_index = int(len(df) * 0.95)
+    train_df = df[:split_index]
+    val_df = df[split_index:]
+    if val_num is not None:
+        val_df = val_df[:val_num]
+
+    if dev_mode:
+        train_df = train_df[:10]
+        val_df = val_df[:10]
+
+    invert_dict = create_invert_dict(train_df.copy())
+    train_set = ImageDataset(train_df, invert_dict, settings.TRAIN_IMG_DIR, stoi, train_mode=True)
+    val_set = ImageDataset(val_df, None, settings.TRAIN_IMG_DIR, stoi, train_mode=False)
+    
+    train_loader = data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4, collate_fn=train_set.collate_fn, drop_last=True)
+    train_loader.num = len(train_set)
+
+    val_loader = data.DataLoader(val_set, batch_size=val_batch_size, shuffle=False, num_workers=4, collate_fn=val_set.collate_fn, drop_last=False)
+    val_loader.num = len(val_set)
+
+    return train_loader, val_loader
+
+
+def test_train_val_loader():
+    train_loader, val_loader = get_train_val_loaders(50000, dev_mode=True, batch_size=2, val_batch_size=2)
+    for img, label in train_loader:
+        print(img.size())
+        print(label.size())
+        print(label)
+        break
+    for img, label in val_loader:
+        print(img.size())
+        print(label.size())
+        print(label)
+        break
+
+def test_index_loader():
+    loader = get_retrieval_index_loader(batch_size=4, dev_mode=True)
+    for img in loader:
+        print(img.size(), img)
+
+if __name__ == '__main__':
+    test_train_val_loader()
+    #test_test_loader()
+    #test_index_loader()
---
+++ recognition/utils.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import pandas as pd
+import torch
+import settings
+
+def predict_model_softmax_output(model, test_loader, cls_out_index=2):
+    preds = []
+    scores = []
+    founds = []
+    cur_num = 0
+    with torch.no_grad():
+        for i, (x, found) in enumerate(test_loader):
+            cur_num += x.size(0)
+            x = x.cuda()
+            output = model(x)
+            cls_out = output if cls_out_index is None else output[cls_out_index]
+            cls_out = F.softmax(cls_out, dim=1)
+            score, pred = cls_out.max(1)
+
+            preds.append(pred)  
+            scores.append(score)
+            founds.append(found)
+            print('{}/{}'.format(cur_num, test_loader.num), end='\r')
+
+    # preds is cls index, not label
+    return [torch.cat(x, 0).numpy() for x in preds, scores, founds]
+
+def create_submission(preds, scores, founds, outfile, classes=None, dev_mode=False):
+    if classes is None:
+        labels = preds
+    else:
+        labels = [classes[x] for x in preds]
+
+    meta = pd.read_csv(os.path.join(settings.DATA_DIR, 'test', 'test.csv'))
+    label_strs = ['{} {:.6f}'.format(i, j) for i, j in zip(labels, scores)]
+
+    for i in range(len(label_strs)):
+        if founds[i] == 0:
+            label_strs[i] = ''
+
+    if dev_mode:
+        meta = meta.iloc[:len(preds)]  # for dev mode
+        print(label_strs[:4])
+    meta['landmarks'] = label_strs
+    meta.to_csv(outfile, index=False, columns=['id', 'landmarks'])
+
